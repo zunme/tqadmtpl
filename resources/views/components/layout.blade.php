542d46a5ad9f565c9f505f59b8ee080f73b7ea0a
Linux User <zunme@nate.com>
2025-05-22 17:40:37 +0900

    fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,11 +10,6 @@
         <script src="https://kit.fontawesome.com/483598c605.js" crossorigin="anonymous"></script>
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
         <link rel="stylesheet" as="style" crossorigin href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css" />
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.7/viewer.min.js" integrity="sha512-lZD0JiwhtP4UkFD1mc96NiTZ14L7MjyX5Khk8PMxJszXMLvu7kjq1sp4bb0tcL6MY+/4sIuiUxubOqoueHrW4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.7/viewer.css" integrity="sha512-9NawOLzuLE2GD22PJ6IPWXEjPalb/FpdH1qMpgXdaDM+0OfxEV75ZCle6KhZi0vM6ZWvMrNnIZv6YnsL+keVmA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
         <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
         @vite(['resources/css/app.css','resources/js/app.js'])
         @fluxAppearance
@@ -50,84 +45,11 @@
                 font-weight: 500;
             }
         </style>
-    </head>
-    <body class="font-pretendard min-h-screen bg-white antialiased text-sm"
-            x-data="{
-                sidebar_collaspe : $store.sidebar.isOpen,
-                view_collaspe:$store.sidebar.isOpen,
-                
-                allway_collaspe : {{config('tqadmtpl.allway_collaspe',false) ? 'true':'false'}},
-                changeCollaspe(){
-                    if( this.sidebar_collaspe ){
-                        this.sidebar_collaspe = false;
-                        this.view_collaspe = false;
-                        $store.sidebar.close()
-                    }else{
-                        this.sidebar_collaspe = true;
-                        this.view_collaspe = true;
-                        $store.sidebar.open()
-                    }
-                },
-                changeMargin(){
-                    let ref = document.getElementById('navbar_top')
-                    let height = ref.offsetHeight
-                    $refs.main_wrap.style.paddingTop=`${height + 1}px`
-                }
-            }"
-            x-init="changeMargin()"
-        >
-        <div class="min-h-svh relative bg-gray-200 overflow-hidden">
-
-            @if( config('tqadmtpl.use_persist',false) )
-                @persist('sidebar')
-                @include('tqadmtpl::navbar')
-                <livewire:tqadm-side-persist :max_width="$max_width" :min_width="$min_width"/>  
-                <div class="bg-gray-400/80 fixed top-0 bottom-0 left-0 right-0"
-                    @click="if(allway_collaspe && !sidebar_collaspe ) {changeCollaspe();}"
-                    x-show="allway_collaspe && !sidebar_collaspe"
-                    >
-                </div>  
-                @endpersist
-            @else 
-                @include('tqadmtpl::navbar')
-                <livewire:tqadm-side :max_width="$max_width" :min_width="$min_width"/>
-                <div class="bg-gray-400/80 fixed top-0 bottom-0 left-0 right-0"
-                    @click="if(allway_collaspe && !sidebar_collaspe ) {changeCollaspe();}"
-                    x-show="allway_collaspe && !sidebar_collaspe"
-                    >
-                </div>  
-            @endif
-            <main class="w-full transition-[width] duration-300" 
-                :class=" allway_collaspe  ? '!{{$min_main}}':'!{{$max_main}}'"
-                x-ref="main_wrap">
-                <div class="p-1">
-                    <div class="bg-white rounded-md px-2 pt-2 {{config('tqadmtpl.use_main_bottom',false) ? 'pb-10':''}}">
-                        {{$slot}}
-                    </div>
-                </div>
-            </main>
-            @if(config('tqadmtpl.use_main_bottom',false)) 
-            <div class="fixed bottom-0 right-0 h-10 w-full {{config('tqadmtpl.top-z-index','z-9')}}"
-                :class=" allway_collaspe  ? '!{{$min_main}}':'!{{$max_main}}'"
-                >
-                <div class="w-full transition-[width] duration-300 h-full shadow-[0_-5px_5px_-5px_#333] {{ config('tqadmtpl.main_bottom_class','')}}" 
-                >
-                    @if( isset($bottom) )
-                        {{$bottom}}
-                    @else 
-                    <div class="bg- flex h-full items-center justify-center text-center">Admin Page</div>
-                    @endif
-                </div>
-            </div>
-            @endif
-        </div>
-        <!-- wire modal -->
-        @livewire('wire-elements-modal')
-        <!-- bukScripts -->
-        <!-- flux -->
-        @fluxScripts
-        @taqScripts
-        <script src="/flux/flux.min.js?id=8da5418c" data-navigate-once></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.7/viewer.min.js" integrity="sha512-lZD0JiwhtP4UkFD1mc96NiTZ14L7MjyX5Khk8PMxJszXMLvu7kjq1sp4bb0tcL6MY+/4sIuiUxubOqoueHrW4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.7/viewer.css" integrity="sha512-9NawOLzuLE2GD22PJ6IPWXEjPalb/FpdH1qMpgXdaDM+0OfxEV75ZCle6KhZi0vM6ZWvMrNnIZv6YnsL+keVmA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        @foreach (  config('tqadmtpl.add_body') as $item)
+            {!! $item !!}
+        @endforeach
         <script>
          document.addEventListener("alpine:init", () => {
             /* single open */
@@ -211,9 +133,84 @@
             img_viewer.show()
         }
         </script>
+    </head>
+    <body class="font-pretendard min-h-screen bg-white antialiased text-sm"
+            x-data="{
+                sidebar_collaspe : $store.sidebar.isOpen,
+                view_collaspe:$store.sidebar.isOpen,
+                
+                allway_collaspe : {{config('tqadmtpl.allway_collaspe',false) ? 'true':'false'}},
+                changeCollaspe(){
+                    if( this.sidebar_collaspe ){
+                        this.sidebar_collaspe = false;
+                        this.view_collaspe = false;
+                        $store.sidebar.close()
+                    }else{
+                        this.sidebar_collaspe = true;
+                        this.view_collaspe = true;
+                        $store.sidebar.open()
+                    }
+                },
+                changeMargin(){
+                    let ref = document.getElementById('navbar_top')
+                    let height = ref.offsetHeight
+                    $refs.main_wrap.style.paddingTop=`${height + 1}px`
+                }
+            }"
+            x-init="changeMargin()"
+        >
+        <div class="min-h-svh relative bg-gray-200 overflow-hidden">
 
-        @foreach (  config('tqadmtpl.add_body') as $item)
-            {!! $item !!}
-        @endforeach
+            @if( config('tqadmtpl.use_persist',false) )
+                @persist('sidebar')
+                @include('tqadmtpl::navbar')
+                <livewire:tqadm-side-persist :max_width="$max_width" :min_width="$min_width"/>  
+                <div class="bg-gray-400/80 fixed top-0 bottom-0 left-0 right-0"
+                    @click="if(allway_collaspe && !sidebar_collaspe ) {changeCollaspe();}"
+                    x-show="allway_collaspe && !sidebar_collaspe"
+                    >
+                </div>  
+                @endpersist
+            @else 
+                @include('tqadmtpl::navbar')
+                <livewire:tqadm-side :max_width="$max_width" :min_width="$min_width"/>
+                <div class="bg-gray-400/80 fixed top-0 bottom-0 left-0 right-0"
+                    @click="if(allway_collaspe && !sidebar_collaspe ) {changeCollaspe();}"
+                    x-show="allway_collaspe && !sidebar_collaspe"
+                    >
+                </div>  
+            @endif
+            <main class="w-full transition-[width] duration-300" 
+                :class=" allway_collaspe  ? '!{{$min_main}}':'!{{$max_main}}'"
+                x-ref="main_wrap">
+                <div class="p-1">
+                    <div class="bg-white rounded-md px-2 pt-2 {{config('tqadmtpl.use_main_bottom',false) ? 'pb-10':''}}">
+                        {{$slot}}
+                    </div>
+                </div>
+            </main>
+            @if(config('tqadmtpl.use_main_bottom',false)) 
+            <div class="fixed bottom-0 right-0 h-10 w-full {{config('tqadmtpl.top-z-index','z-9')}}"
+                :class=" allway_collaspe  ? '!{{$min_main}}':'!{{$max_main}}'"
+                >
+                <div class="w-full transition-[width] duration-300 h-full shadow-[0_-5px_5px_-5px_#333] {{ config('tqadmtpl.main_bottom_class','')}}" 
+                >
+                    @if( isset($bottom) )
+                        {{$bottom}}
+                    @else 
+                    <div class="bg- flex h-full items-center justify-center text-center">Admin Page</div>
+                    @endif
+                </div>
+            </div>
+            @endif
+        </div>
+        <!-- wire modal -->
+        @livewire('wire-elements-modal')
+        <!-- bukScripts -->
+        <!-- flux -->
+        @fluxScripts
+        @taqScripts
+        <script src="/flux/flux.min.js?id=8da5418c" data-navigate-once></script>
+
     </body>
 </html>
--- src/TqadmtplServiceProvider.php
+++ src/TqadmtplServiceProvider.php
@@ -34,7 +34,7 @@ class TqadmtplServiceProvider extends PackageServiceProvider
             ->hasViewComponent('tqadm', Sidebaritem::class)
             ->hasViewComponent('tqadm', Sidebargroup::class)
             ->hasViewComponent('tqadm', Sidebarpersist::class)
-            
+            ->hasAssets()
             ->hasRoute('web')
             ->hasMigration('create_tq_memos_table')
             //->hasCommand(TqadmtplCommand::class);
